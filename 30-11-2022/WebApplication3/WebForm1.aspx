55bd92312bb39471c2930ad544b38b7397c1eb87
momen Abu Alhaija <115459911+MomenHaija@users.noreply.github.com>
2022-11-30 22:33:26 +0300

    first My Code on ASP.NET web form

---
+++ 30-11-2022/WebApplication3/WebForm1.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+&nbsp;&nbsp; Number1:
+            <asp:TextBox ID="Number1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;
+            <br />
+            <br />
+&nbsp;&nbsp; Number2:
+            <asp:TextBox ID="Number2" runat="server" ></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Add" runat="server" Text="+" OnClick="Add_Click1" />
+&nbsp;&nbsp;
+            <asp:Button ID="Sub" runat="server" OnClick="Sub_Click" Text="-" />
+&nbsp;&nbsp;
+            <asp:Button ID="Div" runat="server" OnClick="Div_Click" Text="/" />
+&nbsp;
+            <asp:Button ID="Mult" runat="server" OnClick="Mult_Click" Text="*" />
+&nbsp;<br />
+            <br />
+&nbsp;&nbsp;&nbsp; Result:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Result" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 30-11-2022/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void Add_Click1(object sender, EventArgs e)
+        {
+            int num1, num2, result;
+            num1 = Convert.ToInt32(Number1.Text);
+            num2 = Convert.ToInt32(Number2.Text);
+            result = num1 + num2;
+            Result.Text=Convert.ToString(result);
+        }
+
+        protected void Sub_Click(object sender, EventArgs e)
+        {
+            int num1, num2, result;
+            num1 = Convert.ToInt32(Number1.Text);
+            num2 = Convert.ToInt32(Number2.Text);
+            result = num1 - num2;
+            Result.Text = Convert.ToString(result);
+        }
+
+        protected void Div_Click(object sender, EventArgs e)
+        {
+            int num1, num2, result;
+            num1 = Convert.ToInt32(Number1.Text);
+            num2 = Convert.ToInt32(Number2.Text);
+            result = num1 / num2;
+            Result.Text = Convert.ToString(result);
+        }
+
+
+        protected void Mult_Click(object sender, EventArgs e)
+        {
+            int num1, num2, result;
+            num1 = Convert.ToInt32(Number1.Text);
+            num2 = Convert.ToInt32(Number2.Text);
+            result = num1 * num2;
+            Result.Text = Convert.ToString(result);
+        }
+
+    }
+}
